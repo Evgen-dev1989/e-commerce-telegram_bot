77dfaf983455b17d867581b545d26df59a1f1b24
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2025-06-15 18:48:21 +0300

    keyboard_handler

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 import asyncio
 
+import asyncpg
 import requests
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters import Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from lxml import html
 from lxml.html import HtmlElement
 
-from config import token
+from config import database, host, password, port, token, user
 
 url = 'https://www.omegawatches.com/en-au/suggestions/omega-mens-watches'
 
+import time
+
 import requests
 from lxml import html
-import time
+
+create_db = (
+    """
+        CREATE TABLE IF NOT EXISTS users(
+            id SERIAL,
+            time TIMESTAMP DEFAULT NOW(),
+            user_id INTEGER PRIMARY KEY NOT NULL,
+            user_name VARCHAR(250) NOT NULL,
+            first_name VARCHAR(250),
+            last_name VARCHAR(250)
+        )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS categories (
+            user_id INTEGER,
+            category VARCHAR(100),
+            time TIMESTAMP DEFAULT NOW(),
+            FOREIGN KEY (user_id )
+            REFERENCES users (user_id )
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+)
+    """
+)
+
+async def connect_db():
+
+    return await asyncpg.connect(user=user, password=password, database=database, host=host, port=port)
+
+
 
 def remove_duplicates(watches):
     unique = []
@@ -49,13 +82,35 @@ async def get_watches(url):
 async def start_handler(message: types.Message):
     await message.answer("Welcome to the Omega Watches Bot! Type /watches to see the latest watches.")
 
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="button 1"), KeyboardButton(text="button 2")],
+        [KeyboardButton(text="button 3")]
+    ],
+    resize_keyboard=True
+)
+
+
+async def keyboard_handler(message: types.Message):
+    await message.answer("Select an option:", reply_markup=keyboard)
+
+
+
 async def main():
     start = time.perf_counter()
+    conn = await connect_db()
+
     bot = Bot(token=token)
     dp = Dispatcher()
+
     watches = await get_watches(url)
-    print(f"quantity: {len(watches)}")
+    #print(f"quantity: {len(watches)}")
+
+    await conn.execute(*create_db)
     dp.message.register(start_handler, Command("start"))
+    dp.message.register(keyboard_handler)
+ 
+
     end = time.perf_counter()
     print(f"execution time: {end - start:.6f} seconds")
     await dp.start_polling(bot)
